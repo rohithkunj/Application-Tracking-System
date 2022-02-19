6add2f121fb8cfe57c6dd906d93d6391c9c38da6
Rohith <77923387+rohithkunj@users.noreply.github.com>
2022-02-19 17:39:42 +0530

    added search feature

--- DocRepository.java
+++ DocRepository.java
@@ -1,10 +1,22 @@
 package net.javaguides.springboot.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import net.javaguides.springboot.model.Doc;
+import net.javaguides.springboot.model.Employee;
 
 
 public interface DocRepository  extends JpaRepository<Doc,Integer>{
 
+	
+	@Query(value = "select * from Docs e where e.docName like %:keyword% or e.docType like %:keyword% ", nativeQuery = true)
+	List<Doc> findByKeyword(@Param("keyword") String keyword);
+
+	
 }
+
